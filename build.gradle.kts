a97e7e76fbfea45a6b949b212ae5106c2ddc1f86
MirrexOne <mirrexone@gmail.com>
2024-09-09 11:46:26 +0300

    Update project

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "dev.mirrex"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.mapstruct:mapstruct:1.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Abelovitsky_JavaAdvancedServer"
