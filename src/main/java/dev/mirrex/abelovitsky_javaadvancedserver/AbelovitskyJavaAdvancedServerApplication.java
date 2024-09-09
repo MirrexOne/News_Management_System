568c7e5f57f1dfb121fa6af87c546fdd67f2a9f5
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-09 11:46:26 +0300

    Update project

---
+++ src/main/java/dev/mirrex/abelovitsky_javaadvancedserver/AbelovitskyJavaAdvancedServerApplication.java
@@ -0,0 +1,13 @@
+package dev.mirrex.abelovitsky_javaadvancedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbelovitskyJavaAdvancedServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbelovitskyJavaAdvancedServerApplication.class, args);
+    }
+
+}
