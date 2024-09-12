0518c505903a546a43bd4f95e55d0a75ce128c14
MirrexOne <mirrexone@gmail.com>
2024-09-12 14:24:08 +0300

    Remove redundant methods and variables

--- src/main/java/dev/mirrex/dto/response/CustomSuccessResponse.java
+++ src/main/java/dev/mirrex/dto/response/CustomSuccessResponse.java
@@ -1,19 +1,30 @@
 package dev.mirrex.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.List;
 
 @Getter
 @Setter
-public class CustomSuccessResponse<T> {
+public class CustomSuccessResponse<T> extends BaseSuccessResponse {
 
-    private final T data;
+    private T data;
 
-    private Integer statusCode = 0;
-
-    private Boolean success = true;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<Integer> codes;
 
     public CustomSuccessResponse(T data) {
+        super(1, true);
         this.data = data;
+    }
+
+    public CustomSuccessResponse(List<Integer> codes, boolean success) {
+        super(getStatusCode(codes), success);
+        this.codes = codes;
+    }
+
+    private static int getStatusCode(List<Integer> codes) {
+        return codes.isEmpty() ? 0 : codes.getFirst();
     }
 }
