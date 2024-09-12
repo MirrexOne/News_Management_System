ec7971369ae8a6b4f22bbfcd91279ea474020b6a
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-12 17:39:41 +0300

    Rename DTO's and add one constant

--- src/main/java/dev/mirrex/service/UserService.java
+++ src/main/java/dev/mirrex/service/UserService.java
@@ -1,10 +1,10 @@
 package dev.mirrex.service;
 
-import dev.mirrex.dto.request.LoginUserDtoRequest;
-import dev.mirrex.dto.request.RegisterUserDtoRequest;
+import dev.mirrex.dto.request.LoginUserRequest;
+import dev.mirrex.dto.request.RegisterUserRequest;
 import dev.mirrex.dto.response.CustomSuccessResponse;
 
 public interface UserService {
 
-    CustomSuccessResponse<LoginUserDtoRequest> registerUser(RegisterUserDtoRequest registerUserDtoRequest);
+    CustomSuccessResponse<LoginUserRequest> registerUser(RegisterUserRequest registerUserRequest);
 }
