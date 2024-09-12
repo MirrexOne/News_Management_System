c28290209c8dc311b7854fdff5daa4678f511e09
MirrexOne <mirrexone@gmail.com>
2024-09-12 11:25:02 +0300

    Update UserMapper

--- src/main/java/dev/mirrex/mapper/UserMapper.java
+++ src/main/java/dev/mirrex/mapper/UserMapper.java
@@ -4,11 +4,15 @@ import dev.mirrex.dto.request.RegisterUserDto;
 import dev.mirrex.dto.request.LoginUserDto;
 import dev.mirrex.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
     User toUser(RegisterUserDto registerUserDto);
 
+    @Mapping(target = "token", ignore = true)
     LoginUserDto toLoginUserDto(User user);
 }
