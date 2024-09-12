23405b0e448ebff6d5fad76e41bcfe97ed1759a8
MirrexOne <mirrexone@gmail.com>
2024-09-12 10:59:46 +0300

    Update CSR class

--- src/main/java/dev/mirrex/dto/response/CustomSuccessResponse.java
+++ src/main/java/dev/mirrex/dto/response/CustomSuccessResponse.java
@@ -1,15 +1,17 @@
 package dev.mirrex.dto.response;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class CustomSuccessResponse<T> {
 
-    private T data;
+    private final T data;
 
-    private int statusCode = 0;
+    private Integer statusCode = 0;
 
-    private boolean success = true;
+    private Boolean success = true;
 
     public CustomSuccessResponse(T data) {
         this.data = data;
