d651d2fa4b25dec7d991cf6c3d79748720de7535
MirrexOne <mirrexone@gmail.com>
2024-09-12 14:30:25 +0300

    Refactored

--- src/main/java/dev/mirrex/exception/CustomException.java
+++ src/main/java/dev/mirrex/exception/CustomException.java
@@ -2,30 +2,14 @@ package dev.mirrex.exception;
 
 import dev.mirrex.util.ErrorCode;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    private final HttpStatus status;
-
-    public CustomException(ErrorCode errorCode, HttpStatus status) {
+    public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
-        this.status = status;
-    }
-
-    public CustomException(ErrorCode errorCode) {
-        this(errorCode, HttpStatus.BAD_REQUEST);
-    }
-
-    public Integer getCode() {
-        return errorCode.getCode();
-    }
-
-    public String getMessage() {
-        return errorCode.getMessage();
     }
 }
