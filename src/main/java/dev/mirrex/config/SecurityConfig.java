bba494a634cac6c3c649bb68f7983c1d4a512e32
MirrexOne <mirrexone@gmail.com>
2024-09-13 08:52:37 +0300

    Changed permission of endpoint

--- src/main/java/dev/mirrex/config/SecurityConfig.java
+++ src/main/java/dev/mirrex/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService),
