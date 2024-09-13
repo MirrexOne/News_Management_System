549ea9c95bebf574fdadbabe0f448fbd9fb103b4
MirrexOne <mirrexone@gmail.com>
2024-09-13 11:24:13 +0300

    Remove unused imports and blank lines

--- src/main/java/dev/mirrex/service/UserServiceImpl.java
+++ src/main/java/dev/mirrex/service/UserServiceImpl.java
@@ -13,13 +13,9 @@ import dev.mirrex.repository.UserRepository;
 import dev.mirrex.util.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
