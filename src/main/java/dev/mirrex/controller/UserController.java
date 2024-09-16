71be324e8705304e578bfebf8105691cef54764e
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-16 17:16:24 +0300

    Add @Transactional and change HTTP method in replaceUser method

--- src/main/java/dev/mirrex/controller/UserController.java
+++ src/main/java/dev/mirrex/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,7 +56,7 @@ public class UserController {
                 .body(userService.deleteUser());
     }
 
-    @PostMapping
+    @PutMapping
     public ResponseEntity<CustomSuccessResponse<PutUserResponse>> replaceUser(
             @RequestBody PutUserRequest userNewData) {
         return ResponseEntity.ok()
--- src/main/java/dev/mirrex/service/impl/UserServiceImpl.java
+++ src/main/java/dev/mirrex/service/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
@@ -58,6 +59,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public CustomSuccessResponse<PutUserResponse> replaceUser(PutUserRequest userNewData) {
         User currentAuthedUser = getCurrentUser();
 
