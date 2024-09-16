cc63ba7c82f018e5fca87742e4c4d89b6c63c836
MirrexOne <mirrexone@gmail.com>
2024-09-16 10:19:09 +0300

    Add delete current authorized user function

--- src/main/java/dev/mirrex/controller/UserController.java
+++ src/main/java/dev/mirrex/controller/UserController.java
@@ -1,13 +1,16 @@
 package dev.mirrex.controller;
 
+import dev.mirrex.dto.response.baseResponse.BaseSuccessResponse;
 import dev.mirrex.dto.response.baseResponse.CustomSuccessResponse;
 import dev.mirrex.dto.response.PublicUserResponse;
 import dev.mirrex.service.UserService;
 import dev.mirrex.util.Constants;
+import dev.mirrex.util.ValidationConstants;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +34,8 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomSuccessResponse<PublicUserResponse>> getUserInfoById(
-            @PathVariable @Pattern(regexp = Constants.UUID_REGEX) UUID id) {
+            @PathVariable
+            @Pattern(regexp = Constants.UUID_REGEX, message = ValidationConstants.MAX_UPLOAD_SIZE_EXCEEDED) UUID id) {
         return ResponseEntity.ok()
                 .body(userService.getUserInfoById(id));
     }
@@ -40,5 +44,11 @@ public class UserController {
     public ResponseEntity<CustomSuccessResponse<PublicUserResponse>> getUserInfo() {
         return ResponseEntity.ok()
                 .body(userService.getUserInfo());
+    }
+
+    @DeleteMapping
+    public ResponseEntity<BaseSuccessResponse> deleteUser() {
+        return ResponseEntity.ok()
+                .body(userService.deleteUser());
     }
 }
