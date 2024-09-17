c778cc3387078ecaaf589c72730276c3de328642
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-17 12:32:38 +0300

    Update entities

--- src/main/java/dev/mirrex/entities/News.java
+++ src/main/java/dev/mirrex/entities/News.java
@@ -3,13 +3,15 @@ package dev.mirrex.entities;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.HashSet;
@@ -22,26 +24,27 @@ import java.util.Set;
 public class News {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 3, max = 160)
     @Column(nullable = false)
     private String title;
 
-    @Size(min = 3, max = 160)
     @Column(nullable = false)
     private String description;
 
-    @Size(min = 3, max = 130)
     @Column(nullable = false)
     private String image;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
-        name = "news_tags",
-        joinColumns = @JoinColumn(name = "news_id"),
-        inverseJoinColumns = @JoinColumn(name = "tag_id")
+            name = "news_tags",
+            joinColumns = @JoinColumn(name = "news_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
     private Set<Tag> tags = new HashSet<>();
 }
