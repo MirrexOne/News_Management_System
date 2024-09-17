753289a4a9bbba377bf0af9d50c455abe26d391d
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-17 14:10:52 +0300

    Add deleteNewsById method

--- src/main/java/dev/mirrex/controllers/NewsController.java
+++ src/main/java/dev/mirrex/controllers/NewsController.java
@@ -2,10 +2,15 @@ package dev.mirrex.controllers;
 
 import dev.mirrex.dto.request.NewsCreateRequest;
 import dev.mirrex.dto.response.CreateNewsSuccessResponse;
+import dev.mirrex.dto.response.baseResponse.BaseSuccessResponse;
 import dev.mirrex.services.NewsService;
+import dev.mirrex.util.ValidationConstants;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +27,13 @@ public class NewsController {
     public ResponseEntity<CreateNewsSuccessResponse> createNews(
             @Valid @RequestBody NewsCreateRequest newsDto) {
         CreateNewsSuccessResponse response = newsService.createNews(newsDto);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseSuccessResponse> deleteNewsById(
+            @PathVariable @Min(value = 1, message = ValidationConstants.ID_MUST_BE_POSITIVE) Long id) {
+        BaseSuccessResponse response = newsService.deleteNewsById(id);
         return ResponseEntity.ok().body(response);
     }
 }
