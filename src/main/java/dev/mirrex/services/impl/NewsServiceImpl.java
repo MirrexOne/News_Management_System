4b4f1abb27dcc3c34ab9104c46c027ebcf7a1c10
MirrexOne <mirrexone@gmail.com>
2024-09-17 12:31:57 +0300

    Udpate services

--- src/main/java/dev/mirrex/services/impl/NewsServiceImpl.java
+++ src/main/java/dev/mirrex/services/impl/NewsServiceImpl.java
@@ -4,14 +4,16 @@ import dev.mirrex.dto.request.NewsCreateRequest;
 import dev.mirrex.dto.response.CreateNewsSuccessResponse;
 import dev.mirrex.entities.News;
 import dev.mirrex.entities.Tag;
+import dev.mirrex.entities.User;
 import dev.mirrex.mappers.NewsMapper;
 import dev.mirrex.repositories.NewsRepository;
-import dev.mirrex.repositories.TagRepository;
 import dev.mirrex.services.NewsService;
+import dev.mirrex.services.TagService;
+import dev.mirrex.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -20,29 +22,24 @@ public class NewsServiceImpl implements NewsService {
 
     private final NewsRepository newsRepository;
 
-    private final TagRepository tagRepository;
+    private final TagService tagService;
+
+    private final UserService userService;
 
     private final NewsMapper newsMapper;
 
     @Override
     @Transactional
     public CreateNewsSuccessResponse createNews(NewsCreateRequest newsDto) {
-        News news = newsMapper.toEntity(newsDto);
+        User currentUser = userService.getCurrentUser(SecurityContextHolder.getContext().getAuthentication());
 
-        Set<Tag> tags = new HashSet<>();
-        for (String tagName : newsDto.getTags()) {
-            Tag tag = tagRepository.findByTitle(tagName)
-                    .orElseGet(() -> {
-                        Tag newTag = new Tag();
-                        newTag.setTitle(tagName);
-                        return tagRepository.save(newTag);
-                    });
-            tags.add(tag);
-        }
+        News news = newsMapper.toNews(newsDto);
+        news.setAuthor(currentUser);
+
+        Set<Tag> tags = tagService.getOrCreateTags(newsDto.getTags());
         news.setTags(tags);
 
         News savedNews = newsRepository.save(news);
-
-        return newsMapper.toDto(savedNews);
+        return newsMapper.toCreateNewsResponse(savedNews);
     }
 }
