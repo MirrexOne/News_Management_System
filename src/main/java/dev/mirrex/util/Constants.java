44e83c169072c8bfc9463be1cf697fb15adfa879
MirrexOne <mirrexone@gmail.com>
2024-09-18 14:25:01 +0300

    Delete redundant constant

--- src/main/java/dev/mirrex/util/Constants.java
+++ src/main/java/dev/mirrex/util/Constants.java
@@ -3,7 +3,6 @@ package dev.mirrex.util;
 public interface Constants {
     String AUTHORIZATION = "Authorization";
     String BEARER = "Bearer ";
-    String UNAUTHORIZED = "Unauthorized";
     String UUID_REGEX = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
     String PATH_PATTERN = "^(.+)\\/([^\\/]+)$";
 }
