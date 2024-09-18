0c5cb217cfe9d68ce140cf359a777940c7bb69cf
Alex Belovitsky <a.belovitsky@stv.dunice.net>
2024-09-18 11:22:03 +0300

    Add find news by difference criteria

--- src/main/java/dev/mirrex/repositories/NewsRepository.java
+++ src/main/java/dev/mirrex/repositories/NewsRepository.java
@@ -5,7 +5,16 @@ import dev.mirrex.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+
     Page<News> findByAuthor(User author, Pageable pageable);
+
+    @Query("SELECT DISTINCT n FROM News n LEFT JOIN n.tags t WHERE " +
+            "(:author IS NULL OR n.author.name = :author) AND " +
+            "(:keywords IS NULL OR CAST(n.title AS string) ILIKE CONCAT('%', :keywords, '%') OR CAST(n.description AS string) ILIKE CONCAT('%', :keywords, '%')) AND " +
+            "(:tags IS NULL OR t.title IN :tags)")
+    Page<News> findNewsByFilters(String author, String keywords, List<String> tags, Pageable pageable);
 }
